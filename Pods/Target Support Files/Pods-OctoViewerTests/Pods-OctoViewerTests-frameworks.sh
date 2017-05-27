c9fc07ebbf96bc377b9a3c1fe72b98d40aecc0b0
heshamsalman <hesham.salman@viewthespace.com>
2017-05-26 22:04:38 -0400

    Revert "Removed dependencies"
    This reverts commit a35013a004cac83684f2038983b30809dd739d71.

--- Pods/Target Support Files/Pods-OctoViewerTests/Pods-OctoViewerTests-frameworks.sh
+++ Pods/Target Support Files/Pods-OctoViewerTests/Pods-OctoViewerTests-frameworks.sh
@@ -98,6 +98,8 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Nimble/Nimble.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Quick/Quick.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RxBlocking/RxBlocking.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RxTest/RxTest.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
@@ -109,6 +111,8 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Nimble/Nimble.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Quick/Quick.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RxBlocking/RxBlocking.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RxTest/RxTest.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
